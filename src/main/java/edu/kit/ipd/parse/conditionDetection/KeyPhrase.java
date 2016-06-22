c814d2232cafeeeaed02b2327fd79815fa801e5d
Mathias Landhäußer <mathias.landhaeusser@kit.edu>
2016-06-22 14:45:11 +0200

    Add KeyPhrase class

---
+++ src/main/java/edu/kit/ipd/parse/conditionDetection/KeyPhrase.java
@@ -0,0 +1,9 @@
+package edu.kit.ipd.parse.conditionDetection;
+
+import java.util.ArrayList;
+
+public class KeyPhrase extends ArrayList<String> {
+
+	private static final long serialVersionUID = 1742356971816124462L;
+
+}
