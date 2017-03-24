207c7954058cc5af9d6d447073beecf2105255d9
Tobias Hey <tobias.hey@kit.edu>
2017-03-24 09:26:32 +0100

    implemented easy coref mock for tests

--- src/test/java/edu/kit/ipd/parse/conditionDetection/CoreferenceTest.java
+++ src/test/java/edu/kit/ipd/parse/conditionDetection/CoreferenceTest.java
@@ -2,7 +2,9 @@ package edu.kit.ipd.parse.conditionDetection;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,6 +16,7 @@ import edu.kit.ipd.parse.luna.graph.IArcType;
 import edu.kit.ipd.parse.luna.graph.IGraph;
 import edu.kit.ipd.parse.luna.graph.INode;
 import edu.kit.ipd.parse.luna.graph.INodeType;
+import edu.kit.ipd.parse.luna.graph.Pair;
 import edu.kit.ipd.parse.luna.graph.ParseNode;
 import edu.kit.ipd.parse.shallownlp.ShallowNLP;
 
@@ -52,7 +55,7 @@ public class CoreferenceTest {
 		dt.init();
 
 		input = "if I leave do the laundry iron it go to the wardrobe and put it in the wardrobe";
-
+		//input = "Robo go to the table if there are any dirty dishes got the dirty dishes and go to the dishwasher open the dishwasher and put the dirty dishes into the dishwasher the dishwasher and returned to the table shower and clean this up the clean dishes and go to the keyboard open the card and put the clean dishes into the car";
 		try {
 			actual = snlp.parse(input, null);
 		} catch (IOException | URISyntaxException | InterruptedException e) {
@@ -81,28 +84,45 @@ public class CoreferenceTest {
 		// Add synthetic coref-arc
 		INode corefBegin = null;
 		INode corefEnd = null;
-		IArcType arcType;
-		for (INode node : graph.getNodes()) {
-			if (node.getAttributeValue("value").toString().equalsIgnoreCase("it") && corefEnd == null) {
-				corefEnd = node;
-			} else if (node.getAttributeValue("value").toString().equalsIgnoreCase("it")) {
-				corefBegin = node;
-			}
-		}
 		INodeType eNT = graph.createNodeType("contextEntity");
-		INode begin = graph.createNode(eNT);
-		INode end = graph.createNode(eNT);
 		IArcType ref = graph.createArcType("reference");
 		IArcType coref = graph.createArcType("contextRelation");
 		coref.addAttributeToType("double", "confidence");
-		coref.addAttributeToType("String", "name");
+		coref.addAttributeToType("String", "typeOfRelation");
 
-		IArc refArc = graph.createArc(begin, corefBegin, ref);
-		IArc refArc2 = graph.createArc(end, corefEnd, ref);
+		List<Pair<Integer, Integer>> corefs = new ArrayList<>();
+		corefs.add(new Pair<Integer, Integer>(14, 7));
+		//		corefs.add(new Pair<Integer, Integer>(14, 10));
+		//		corefs.add(new Pair<Integer, Integer>(22, 19));
+		//		corefs.add(new Pair<Integer, Integer>(27, 14));
+		//		corefs.add(new Pair<Integer, Integer>(30, 22));
+		//		corefs.add(new Pair<Integer, Integer>(32, 30));
+		//		corefs.add(new Pair<Integer, Integer>(37, 4));
+		//		corefs.add(new Pair<Integer, Integer>(45, 41));
+		//		corefs.add(new Pair<Integer, Integer>(53, 50));
+		//		corefs.add(new Pair<Integer, Integer>(58, 45));
+		//		corefs.add(new Pair<Integer, Integer>(61, 53));
 
-		IArc arc = graph.createArc(begin, end, coref);
-		arc.setAttributeValue("confidence", 1.0d);
-		arc.setAttributeValue("name", "referentRelation");
+		for (Pair<Integer, Integer> pair : corefs) {
+			for (INode node : graph.getNodes()) {
+				if (node.getType().equals(graph.getNodeType("token"))) {
+					if (((Integer) node.getAttributeValue("position")) == pair.getLeft().intValue()) {
+						corefBegin = node;
+					} else if (((Integer) node.getAttributeValue("position")) == pair.getRight().intValue()) {
+						corefEnd = node;
+					}
+				}
+			}
+			INode begin = graph.createNode(eNT);
+			INode end = graph.createNode(eNT);
+			IArc refArc = graph.createArc(begin, corefBegin, ref);
+			IArc refArc2 = graph.createArc(end, corefEnd, ref);
+
+			IArc arc = graph.createArc(begin, end, coref);
+			arc.setAttributeValue("confidence", 1.0d);
+			arc.setAttributeValue("typeOfRelation", "referentRelation");
+		}
+
 		dt.exec();
 		ParseNode[] nodes = dt.getGraph().getNodes().toArray(new ParseNode[0]);
 		for (int i = 3; i < nodes.length; i++) {
